ecb036eee246c6d808609d4a49cb1535be606a29
javier <javier@formatinternet.com>
2023-07-07 11:33:01 +0200

    app_monitorign sample fully functional

--- ingestion/python/tsbs_send/app_monitoring_ingestion.py
+++ ingestion/python/tsbs_send/app_monitoring_ingestion.py
@@ -4,11 +4,13 @@ from faker.providers import DynamicProvider
 from faker.providers import internet
 import os
 import sys
+import time
 
 HOST = os.getenv('QDB_CLIENT_HOST', '127.0.0.1')
 PORT = os.getenv('QDB_CLIENT_PORT', 8812)
 PG_USER = os.getenv('QDB_CLIENT_PG_USER', 'admin')
 PG_PASSWORD = os.getenv('QDB_CLIENT_PG_PASSWORD', 'quest')
+DELAY = 0.1
 
 
 def get_fake_generator():
@@ -20,7 +22,7 @@ def get_fake_generator():
         "/stock/sell",
         "/stock/check",
         "/user/profile",
-        "contract/download"
+        "/contract/download"
         ],
 )
     fake_generator = Faker()
@@ -33,15 +35,15 @@ def fake_row(fake_generator):
     row=dict()
     profile = fake_generator.profile()
     row['name'] = profile['name']
-    row['user_name'] = profile['username']
+    row['username'] = profile['username']
     row["email"] = profile['mail']
     row['company'] = profile['company']
     row['app_action'] = fake_generator.app_action()
-    row['credit_card_provider'] = 'NULL'
+    row['credit_card_provider'] = None
     if row['app_action'] == "/stock/buy":
         row['credit_card_provider'] = fake_generator.credit_card_provider()
-    row['file_name'] = 'NULL'
-    if row['app_action'] == "contract/download":
+    row['file_name'] = None
+    if row['app_action'] == "/contract/download":
         row['file_name'] = fake_generator.file_path(extension='pdf')
     row['ip']=fake_generator.ipv4_private()
     row['method']=fake_generator.http_method()
@@ -49,6 +51,7 @@ def fake_row(fake_generator):
     row['country_code'] = fake_generator.country_code()
     row['time_ms'] = fake_generator.pyint(min_value=20, max_value=1500)
     row['error'] = fake_generator.pybool(truth_probability=8)
+    row['timestamp'] = timestamp = time.time_ns() // 1000
     return row
 
 if __name__ == '__main__':
@@ -57,24 +60,62 @@ if __name__ == '__main__':
         with connection.cursor() as cur:
             cur.execute('''
             CREATE TABLE IF NOT EXISTS app_monitor(
+                timestamp TIMESTAMP,
                 name STRING,
                 username SYMBOL capacity 256 CACHE,
                 email STRING,
                 company STRING,
                 app_action SYMBOL capacity 10 CACHE,
+                method SYMBOL capacity 10 CACHE,
+                time_ms LONG,
                 credit_card_provider SYMBOL capacity 10 CACHE,
                 file_name STRING,
                 ip STRING,
-                method SYMBOL capacity 10 CACHE,
-                user_agent SYMBOL capacity 50 CACHE,
                 country_code SYMBOL capacity 150 CACHE,
-                time_ms LONG,
-                timestamp TIMESTAMP
+                user_agent SYMBOL capacity 50 CACHE
                 ) TIMESTAMP (timestamp) PARTITION BY DAY WAL;
             '''
             )
 
             fg = get_fake_generator()
-            print(fake_row(fg))
+            while True:
+                row=fake_row(fg)
+                cur.execute('''
+                    INSERT INTO app_monitor(
+                        name,
+                        username,
+                        email,
+                        company,
+                        app_action,
+                        credit_card_provider,
+                        file_name,
+                        ip,
+                        method,
+                        user_agent,
+                        country_code,
+                        time_ms,
+                        timestamp
+                        )
+                    VALUES (
+                        %(name)s,
+                        %(username)s,
+                        %(email)s,
+                        %(company)s,
+                        %(app_action)s,
+                        %(credit_card_provider)s,
+                        %(file_name)s,
+                        %(ip)s,
+                        %(method)s,
+                        %(user_agent)s,
+                        %(country_code)s,
+                        %(time_ms)s,
+                        %(timestamp)s
+                    )
+                    '''
+                    , row
+                    )
+                sys.stdout.write(f'sent : {row}\n')
+                time.sleep(DELAY)
+
 
 
